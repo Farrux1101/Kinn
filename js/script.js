9ef4957c5944dc02a65b8dcf4790d52a0f94e677
Farrux0609 <farrux.narzullayev.00@bk.ru>
2021-12-11 12:53:56 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Can also be included with a regular script tag
+import Typed from 'typed.js';
+
+let options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+let typed = new Typed('.element', options);
